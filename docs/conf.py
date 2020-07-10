a3ff8f5ddfcf44e3faecbca2a82f5ca37d6248cc
ktorch <tom.fergson@gmail.com>
2020-07-10 11:47:26 -0400

    initial documentation

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-import pytorch_sphinx_theme
+#import os
+#import sys
+#import pytorch_sphinx_theme
 
 # import os
 # import sys
@@ -48,8 +48,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = 'pytorch_sphinx_theme'
+html_theme = 'alabaster'
+#html_theme = 'pytorch_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
