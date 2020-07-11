f50726d8d618c369e11e9e77d50f5867a504dca4
ktorch <tom.fergson@gmail.com>
2020-07-11 09:49:03 -0400

    initial documentation

--- docs/conf.py
+++ docs/conf.py
@@ -50,6 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 #html_theme = 'alabaster'
 html_theme = 'pytorch_sphinx_theme'
+html_theme_path = [pytorch_sphinx_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
